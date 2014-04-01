2695ff0459eb5f8e49d0dbc3e6387ed766e40b5a
Richard Barran <richard@arbee-design.co.uk>
2014-04-01 11:05:35 +0100

    Sitemap should not include redirect pages.

--- cms/sitemaps/cms_sitemap.py
+++ cms/sitemaps/cms_sitemap.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib.sitemaps import Sitemap
 
+
 def from_iterable(iterables):
     """
     Backport of itertools.chain.from_iterable
@@ -9,15 +10,24 @@ def from_iterable(iterables):
         for element in it:
             yield element
 
+
 class CMSSitemap(Sitemap):
     changefreq = "monthly"
     priority = 0.5
 
     def items(self):
+        """Django-cms has a wee bug in that pages that are just placeholders
+        for a redirect are included in the sitemap.xml - this is the cure.
+        (Unfortunately there's no point in wrapping up this code and sending in
+        a pull request, as it's been substantially rewritten - but still has
+        the same bug!)
+        """
         from cms.utils.page_resolver import get_page_queryset
         page_queryset = get_page_queryset(None)
         all_pages = page_queryset.published().filter(login_required=False)
-        return all_pages
+        return [page
+                for page in all_pages
+                if not page.get_redirect()]
 
     def lastmod(self, page):
         modification_dates = [page.changed_date, page.publication_date]
@@ -26,4 +36,3 @@ class CMSSitemap(Sitemap):
         plugin_modification_dates = map(lambda plugin: plugin.changed_date, plugins)
         modification_dates.extend(plugin_modification_dates)
         return max(modification_dates)
-    
